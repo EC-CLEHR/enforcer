ffa38dd0c9aa0be35c37160512253bf0c00b3b85
EC-CLEHR <161497678+EC-CLEHR@users.noreply.github.com>
2024-07-11 12:30:41 -0500

    cleaning up repo

--- pythonProject/src/lib/helper/api_constants.py
+++
@@ -1,5 +0,0 @@
-from src.lib.helper.path_helper import get_config_ini_file
-import os
-
-config = get_config_ini_file('../api_config.ini')
-TIMEOUT_M = config.getint('TIMEOUT', 'timeout_m')
--- pythonProject/src/lib/helper/get_webdriver.py
+++
@@ -1,9 +0,0 @@
-import chromedriver_autoinstaller
-from selenium import webdriver
-
-opt = webdriver.ChromeOptions()
-opt.add_argument("--start-maximized")
-
-chromedriver_autoinstaller.install()
-driver = webdriver.Chrome(options=opt)
-driver.get('https://stackoverflow.com/')
